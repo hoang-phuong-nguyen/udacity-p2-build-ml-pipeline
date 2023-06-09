f851a72b2bc99e39e62d319e71d4f82f873bc40c
Phuong Nguyen <phuongnguyen.cse@gmail.com>
2023-06-09 11:04:30 +0700

    Fix data cleaning bug in 1.0.0

---
+++ scripts/test_model_with_sample2.sh
@@ -0,0 +1,3 @@
+mlflow run https://github.com/hoang-phuong-nguyen/udacity-p2-build-ml-pipeline.git \
+    -v 1.0.0 \
+    -P hydra_options="etl.sample='sample2.csv'"
--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,11 @@ def go(args):
     
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
+    
+    # Drop rows in the dataset that are not in the proper geolocation
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     df.to_csv("./output/clean_sample.csv", index=False)
     logger.info("Save the new artifact as clean_sample.csv")
     
